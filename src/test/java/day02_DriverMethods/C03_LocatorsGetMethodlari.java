42fc1b0a18294f1ee82c6f2f844542cc8065f863
kkbraslnn <kkaratash@icloud.com>
2023-05-15 21:42:18 +0200

    v2.1

---
+++ src/test/java/day02_DriverMethods/C03_LocatorsGetMethodlari.java
@@ -0,0 +1,29 @@
+package day02_DriverMethods;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C03_LocatorsGetMethodlari {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","/Users/kubrakaratash/IdeaProjects/Seleniumm/src/main/resources/drivers/chromedriver_mac64-2");
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        driver.get("https://www.amazon.com");
+        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("iphone", Keys.ENTER);
+        WebElement aramasonucyazisi = driver.findElement(By.className("sg-col-inner"));
+        System.out.println(aramasonucyazisi.getText());
+        driver.close();
+
+
+
+    }
+}
